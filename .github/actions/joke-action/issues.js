c53bf54c805b275c7f3b5c8f437ebc53017dec1f
Eduard Maura i Puig <edu.maura.puig@gmail.com>
2025-07-16 09:45:58 +0200

    Comment joke on issue

---
+++ .github/actions/joke-action/issues.js
@@ -0,0 +1,20 @@
+const github = require("@actions/github");
+const core = require("@actions/core");
+
+const token = core.getInput("GITHUB_TOKEN", { required: true });
+const octokit = new github.getOctokit(token);
+
+function commentIssue(joke) {
+    const repo = core.getInput("repo", { required: true });
+    const owner = core.getInput("owner", { required: true });
+    const issue_number = core.getInput("issue_number", { required: true });
+
+    return octokit.rest.issues.createComment({
+        owner: owner,
+        repo: repo,
+        issue_number: issue_number,
+        body: joke,
+    })
+}
+
+module.exports = commentIssue;
--- .github/actions/joke-action/main.js
+++ .github/actions/joke-action/main.js
@@ -1,10 +1,17 @@
-const getJoke = require("./joke");
 const core = require("@actions/core");
+const getJoke = require("./joke");
+const commentIssue = require("./issues");
 
 async function run() {
     const joke = await getJoke();
     console.log(joke);
     core.setOutput("joke-output", joke);
+
+    return commentIssue(joke).then((response) => {
+        core.info(`Issue comment: ${response.body.html_url}`);
+    }).catch((error) => {
+        core.setFailed(`Error commenting on PR: ${error.message}`);
+    })
 }
 
 run();
